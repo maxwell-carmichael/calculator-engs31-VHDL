224b332d569b3076c9c6c1e8b489966ed9b0a880
Maxwell J. Carmichael <f003md3@plank.thayer.dartmouth.edu>
2021-05-28 21:17:46 -0400

    initial commit

---
+++ Calculator_Shell.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/29/2021 01:13:44 AM
+-- Design Name: 
+-- Module Name: Calculator_Shell - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Calculator_Shell is
+--  Port ( );
+end Calculator_Shell;
+
+architecture Behavioral of Calculator_Shell is
+
+begin
+
+
+end Behavioral;
